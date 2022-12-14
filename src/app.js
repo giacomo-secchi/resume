dd0a2f0f43ea7dda909d61405076ed51e2f991b4
giacomo.secchi <giacomosecchi@gmail.com>
2022-12-14 15:14:48 +0100

    Setup reload js and templating

---
+++ src/app.js
@@ -0,0 +1,43 @@
+const express = require( 'express' );
+const fs = require( 'fs' );
+const glob = require( 'glob' );
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.set('view engine', 'pug');
+
+
+languages = {};
+
+
+glob.sync( './languages/*.json' ).forEach( ( file ) => {
+  const regex = RegExp('languages/(.*).json', 'g');  
+  let ISOcode = regex.exec(file)[1];
+
+  fs.readFile(file, function(err, data) {
+    languages[ISOcode] = JSON.parse( data.toString() );
+  });
+
+});
+
+app.get( '/:lang', ( req, res ) => {
+  let currentLang = req.params['lang'];
+   
+  if ( languages.hasOwnProperty(currentLang) ) {
+    for (var key of Object.keys(languages[currentLang])) {
+      
+     
+      let pattern = new RegExp("{{" + key + "}}", "g");
+     
+
+      // dataString = dataString.replace(pattern, languages[lang][key]);
+
+      res.render('index', { title: 'Hey', message: 'Hello there!' })
+    }
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});

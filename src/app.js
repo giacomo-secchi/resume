e80ef275a6ddf927fa10a2769e1cc08abe3270f0
Giacomo Secchi <you@example.com>
2022-12-15 22:19:46 +0100

    Enable fetch

--- src/app.js
+++ src/app.js
@@ -1,13 +1,29 @@
 const express = require( 'express' );
 const fs = require( 'fs' );
 const glob = require( 'glob' );
-fetch("http://example.com/api/endpoint")
-  .then((response) => {
-    // Do something with response
-  })
-  .catch(function (err) {
-    console.log("Unable to fetch -", err);
-  });
+
+
+
+// fetch("http://example.com/api/endpoint", {
+//   method: 'POST',
+//   mode: 'cors', // no-cors, *cors, same-origin
+//   cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+//   credentials: 'same-origin', // include, *same-origin, omit
+//   headers: {
+//     'Content-Type': 'application/json'
+//   },
+//   redirect: 'follow', // manual, *follow, error
+//   referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+//   body: JSON.stringify(data) // body data type must match "Content-Type" header
+// })
+// .then((body) => {
+//   fs.writeFile('doc_raptor_sample.pdf', body, "binary", function(writeErr) {
+//     console.log('Saved!');
+//   });
+// })
+//   .catch(function (err) {
+//     console.log("Unable to fetch -", err);
+//   });
 
 
 const app = express();

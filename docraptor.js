03975cde359055636369d58e13225a4873647092
giacomo.secchi <giacomosecchi@gmail.com>
2022-12-20 09:52:03 +0100

    Setup DocRaptor

--- docraptor.js
+++ docraptor.js
@@ -1,69 +1,69 @@
-// const fs = require( 'fs' );
-// var content = "<html><body>TEST!</body></html>";
-
-// config = {
-//   method: 'POST',
-// //  encoding: null, //IMPORTANT! This produces a binary body response instead of text
-//   headers: {
-//     'Content-Type': 'application/json'
-//   },
-//   body: JSON.stringify({
-//     user_credentials: "YOUR_API_KEY_HERE",
-//     mode: 'cors',
-//     cache: 'default',
-//     doc: {
-//       document_content: content,
-//       type: "pdf",
-//       test: true,  // test documents are free but watermarked
-//       prince_options: {
-//         media:   "screen",          // use screen styles instead of print styles
-//         // baseurl: "http://hello.com" // URL to use for generating absolute URLs for assets from relative URLs
-//       }
-//     }
-//   })
-// };
-
-// fetch('https://api.docraptor.com/docs', config)
-// .then(response => response.arrayBuffer())
-//   .then((data) => {
-//     fs.writeFile('doc_raptor_sample.pdf', data, "binary", function(writeErr) {
-//       console.log('Saved!');
-//     });
-//   })
-//   .catch(function (err) {
-//     console.log("Unable to fetch -", err);
-//   });
- 
-var request = require('request');
-var fs = require('fs');
+const fs = require( 'fs' );
 var content = "<html><body>TEST!</body></html>";
 
 config = {
-  url: 'https://api.docraptor.com/docs',
-  encoding: null, //IMPORTANT! This produces a binary body response instead of text
+  method: 'POST',
+//  encoding: null, //IMPORTANT! This produces a binary body response instead of text
   headers: {
     'Content-Type': 'application/json'
   },
-  json: {
+  body: JSON.stringify({
     user_credentials: "YOUR_API_KEY_HERE",
+    mode: 'cors',
+    cache: 'default',
     doc: {
-      // document_content: content,
-      document_url: 'https://personal-website2.herokuapp.com/en',
+      document_content: content,
       type: "pdf",
-      test: true,
+      test: true,  // test documents are free but watermarked
       prince_options: {
-        javascript: true,
-        baseurl: "https://personal-website2.herokuapp.com" // URL to use for generating absolute URLs for assets from relative URLs
+        media:   "screen",          // use screen styles instead of print styles
+        // baseurl: "http://hello.com" // URL to use for generating absolute URLs for assets from relative URLs
       }
     }
-  }
+  })
 };
 
-request.post(config, function(err, response, body) {
-  fs.writeFile('doc_raptor_sample.pdf', body, "binary", function(writeErr) {
-    console.log('Saved!');
+fetch('https://api.docraptor.com/docs', config)
+.then(response => response.arrayBuffer())
+  .then((data) => {
+    fs.writeFile('doc_raptor_sample.pdf', data, "binary", function(writeErr) {
+      console.log('Saved!');
+    });
+  })
+  .catch(function (err) {
+    console.log("Unable to fetch -", err);
   });
-});
+ 
+// var request = require('request');
+// var fs = require('fs');
+// var content = "<html><body>TEST!</body></html>";
+
+// config = {
+//   url: 'https://api.docraptor.com/docs',
+//   encoding: null, //IMPORTANT! This produces a binary body response instead of text
+//   headers: {
+//     'Content-Type': 'application/json'
+//   },
+//   json: {
+//     user_credentials: "YOUR_API_KEY_HERE",
+//     doc: {
+//       // document_content: content,
+//       document_url: 'https://personal-website2.herokuapp.com/en',
+//       type: "pdf",
+//       test: true,
+//       javascript: true,
+//       prince_options: {
+//         baseurl: "https://personal-website2.herokuapp.com" // URL to use for generating absolute URLs for assets from relative URLs
+//       }
+//     }
+//   }
+// };
+
+// request.post(config, function(err, response, body) {
+//   fs.writeFile('doc_raptor_sample.pdf', body, "binary", function(writeErr) {
+//     console.log('Saved!');
+//   });
+// });
  
 //     $doc->setDocumentContent(file_get_contents("index.html"));
 //     # $doc->setDocumentUrl("https://docraptor.com/examples/invoice.html");
--- old/index.php
+++ old/index.php
@@ -799,40 +799,7 @@ affiancamento agli operatori della web agency per lo sviluppo dei progetti, sopr
 
 </body>
 </html>
-
-<!doctype html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Giacomo Secchi</title>
-</head>
-<body>
-	<p>Coming Soon</p>
-<script type="text/javascript">
-
-  // var _gaq = _gaq || [];
-  // _gaq.push(['_setAccount', 'UA-44636069-1']);
-  // _gaq.push(['_trackPageview']);
-
-  // (function() {
-  //   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  //   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-  //   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  // })();
-
-</script>
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-44636069-2', 'fast-forest-9133.herokuapp.com');
-  ga('send', 'pageview');
-
-</script>
-</body>
-</html>
+ 
 
 <!doctype html>
 <html lang="en">
@@ -1077,16 +1044,7 @@ affiancamento agli operatori della web agency per lo sviluppo dei progetti, sopr
 </body>
 </html>
 
-<!doctype html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Giacomo Secchi</title>
-</head>
-<body>
-	<p>Coming Soon</p>
-<script type="text/javascript">
-
+ 
   // var _gaq = _gaq || [];
   // _gaq.push(['_setAccount', 'UA-44636069-1']);
   // _gaq.push(['_trackPageview']);

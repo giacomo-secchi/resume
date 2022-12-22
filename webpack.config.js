848e745447d4386130fa4dbd8556b4b77dc90ce5
giacomo.secchi <giacomosecchi@gmail.com>
2022-12-22 16:12:19 +0100

    Generate styles files

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,30 @@
 const path = require('path');
-
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 
 module.exports = {
-  entry: './src/js/main.js',
+  // entry: './src/js/main.js',
+  entry: {
+    print: path.resolve(__dirname, './src/scss/print.scss'),
+    styles: path.resolve(__dirname, './src/scss/styles.scss')
+  },
   output: {
-    filename: 'main.js',
+    // filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "css/[name].min.css",
+    }),
+  ],
   module: {
     rules: [
       {
         test: /\.s[ac]ss$/i,
         use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
+          MiniCssExtractPlugin.loader,
           // Translates CSS into CommonJS
           "css-loader",
           // Compiles Sass to CSS

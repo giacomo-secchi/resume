035e5f68c9c139da65f46c3c4e3e39ee1a76a535
Giacomo Secchi <you@example.com>
2023-01-05 17:32:18 +0100

    Set error handling

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
+const createError = require('http-errors');
 const express = require('express');
-var path = require('path');
+const path = require('path');
 const port = process.env.PORT || 3000;
 
 let indexRouter = require('./routes/index');
@@ -14,13 +15,28 @@ app.set('view engine', 'pug');
 
 app.set('view engine', 'pug');
 
-app.use(express.static('dist'));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'dist')));
 app.use('/languages', express.static('languages'));
 
-app.get('/:en', indexRouter);
 
+app.use('/', indexRouter);
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)

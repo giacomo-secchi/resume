f3461cef2b84678f87a7776ba25d516f1b5082c3
giacomo.secchi <giacomosecchi@gmail.com>
2023-01-16 10:20:46 +0100

    Remove unused js

--- src/js/main.js
+++ src/js/main.js
@@ -9,15 +9,4 @@ document.addEventListener('DOMContentLoaded', () => {
 
 });
 
-
-function bindLocaleSwitcher(initialValue) {
-    const switcher =
-      document.querySelector("[data-i18n-switcher]");
-    
-    switcher.value = initialValue;
-    switcher.onchange = (e) => {
-      // Set the locale to the selected option[value]
-      // setLocale(e.target.value);
-    };
-}
  
